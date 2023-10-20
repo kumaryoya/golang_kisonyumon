d57d7e86944e5b61c42b8daab44f19726803e6b5
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-10-20 16:21:29 +0900

    セクション8終了

---
+++ section8/main7.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// defer
+
+func TestDefer() {
+	defer fmt.Println("END")
+	fmt.Println("START")
+}
+
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+}
+
+func main() {
+	TestDefer()
+
+	// defer func() {
+		// fmt.Println("1")
+		// fmt.Println("2")
+		// fmt.Println("3")
+	// }()
+
+	fmt.Println("4")
+
+	RunDefer()
+
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	file.Write([]byte("Hello"))
+}
